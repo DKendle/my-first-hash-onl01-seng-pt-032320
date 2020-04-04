e32a44ce8d0dceb0231f3c7abb0b471f1e37cb24
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 02:23:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  kindergarden = {"table"=>"4","Crayons" =>"10","chalkboards"=>"10"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
