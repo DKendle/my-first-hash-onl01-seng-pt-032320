52c5dc8814af7d9655b5ca103743cbb29b694700
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 02:31:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,21 +23,3 @@ def retrieval
 
 end
 
-#def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  shipping_manifest["muskets"] => "2" # add 2 muskets to the shipping_manifest hash below
-
-
-
-  shipping_manifest["gun_powder"] =>"4"
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-#end
