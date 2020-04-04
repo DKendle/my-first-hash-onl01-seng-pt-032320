25b36afd93ea0e64ba8a7596486162b847e18c33
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 02:33:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] => "2" 
-  shipping_manifest["gun_powder"] => "4"
+  shipping_manifest["gun powder"] => "4"
   
 shipping_manifest
   # return the shipping_manifest hash below
